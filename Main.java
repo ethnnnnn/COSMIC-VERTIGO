e455fd069a58e4e756391956e585338aa31dca67
Ethan <289093412+ethnnnnn@users.noreply.github.com>
2026-07-19 14:31:58 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,3 @@
-// COSMIC VERTIGO - a game by Ethan Mason and Henry Wulterkens
-// notes/plans/thought processes on Plans doc
-// thanks for reading/playing, and enjoy! :)
-
 import java.util.*;
 import java.io.*;
 public class Main {
@@ -9,7 +5,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         // title/intro screens
-        System.out.print("   ____ ___  ____  __  __ ___ ____  __     _______ ____ _____ ___ ____  ___  \n  / ___/ _ \\/ ___||  \\/  |_ _/ ___| \\ \\   / / ____|  _ \\_   _|_ _/ ___|/ _ \\ \n | |  | | | \\___ \\| |\\/| || | |      \\ \\ / /|  _| | |_) || |  | | |  _| | | |\n | |__| |_| |___) | |  | || | |___    \\ V / | |___|  _ < | |  | | |_| | |_| |\n  \\____\\___/|____/|_|  |_|___\\____|    \\_/  |_____|_| \\_\\|_| |___\\____|\\___/\n\n[KOZ-mik VUR-ti-goh]\n/\u02C8k\u0251:z.m\u026Ak \u02C8v\u025Dt\u026A\u02CCgo\u01B1 /\n(noun)\n\n1. a feeling of dizziness or awe experienced when contemplating the sheer scale of the universe.\n\n\u001B[1m2. a game by Henry Wulterkens and Ethan Mason.\u001B[0m\n\n************************************\n\nWelcome, Explorer.\n\nWhat is your name? ");
+        System.out.print("   ____ ___  ____  __  __ ___ ____  __     _______ ____ _____ ___ ____  ___  \n  / ___/ _ \\/ ___||  \\/  |_ _/ ___| \\ \\   / / ____|  _ \\_   _|_ _/ ___|/ _ \\ \n | |  | | | \\___ \\| |\\/| || | |      \\ \\ / /|  _| | |_) || |  | | |  _| | | |\n | |__| |_| |___) | |  | || | |___    \\ V / | |___|  _ < | |  | | |_| | |_| |\n  \\____\\___/|____/|_|  |_|___\\____|    \\_/  |_____|_| \\_\\|_| |___\\____|\\___/\n\n[KOZ-mik VUR-ti-goh]\n/\u02C8k\u0251:z.m\u026Ak \u02C8v\u025Dt\u026A\u02CCgo\u01B1 /\n(noun)\n\n1. a feeling of dizziness or awe experienced when contemplating the sheer scale of the universe.\n\n\u001B[1m2. a game by hanktwin and ethnnnnn.\u001B[0m\n\n************************************\n\nWelcome, Explorer.\n\nWhat is your name? ");
         User larry = new User(input.nextLine());
         System.out.print("\nWELCOME, " + larry.getName() + ".\nWould you like a tutorial before you begin? (y/n): ");
         if (M.equalsY(input.nextLine())) {
